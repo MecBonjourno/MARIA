613ac2f41f12c7d38c085580b5abb03a330e93e3
Guilherme Zago <contatogzii@gmail.com>
2023-11-27 22:30:00 -0300

    disclaimer modal finished

--- src/components/Disclaimer/index.tsx
+++ src/components/Disclaimer/index.tsx
@@ -19,19 +19,33 @@ const DisclaimerModal = () => {
 
 	return (
 		isVisible && (
-			<div className="disclaimer-modal text-black">
+			<div className="disclaimer-modal text-black max-h-screen">
 				<div className="disclaimer-content">
-					<h1 className="text-2xl font-bold my-4">Disclaimer - Informações sobre o uso da MARIA</h1>
-					<p>Por favor, leia atentamente antes de usar a MARIA:</p>
+					<h1 className="text-3xl font-bold my-4">Disclaimer - Informações sobre o uso da MARIA</h1>
+					<p className="text-2xl text-red-600 font-medium my-4">Por favor, leia atentamente antes de usar a MARIA:</p>
 					<ul>
-						<li>A MARIA é um assistente virtual em fase Alpha de testes e desenvolvimento.</li>
-						<li>
-							As informações fornecidas pela MARIA não são aprovadas por órgãos públicos ou homologadas oficialmente.
+						<li className="my-2">
+							A MARIA é um assistente virtual de <span className="font-bold">saúde</span> em fase Alpha de testes e
+							desenvolvimento.
 						</li>
-						<li>
-							As respostas da MARIA podem estar incorretas e não devem substituir aconselhamento médico profissional.
+						<li className="my-2">
+							As informações fornecidas pela MARIA{' '}
+							<span className="font-bold">
+								não são aprovadas por qualquer órgão público nacional ou homologadas oficialmente.{' '}
+							</span>
 						</li>
-						<li>Ao enviar qualquer mensagem, você aceita os termos de uso do software.</li>
+						<li className="my-2">
+							Ao Iniciar uma conversa enviando qualquer mensagem, você aceita os nossos{' '}
+							<a href="#" className="text-blue-500">
+								termos de uso do software.
+							</a>
+						</li>
+						<li className="my-2">
+							As respostas da MARIA{' '}
+							<span className="font-bold">
+								podem não estar corretas e não devem substituir aconselhamento médico profissional.
+							</span>
+						</li>
 					</ul>
 					<button
 						className={`bg-green-500 text-white font-bold py-2 px-6 mt-6 rounded hover:bg-green-600 transition duration-300 ease-in-out ${
